8c3dae69f074f7a6ae79d406996b9858a8ad4de3
RafaelCruz25 <rahrulez@hotmail.com>
2021-01-10 13:59:16 -0300

    Ajustes comentarios da PR

--- src/main/kotlin/com/example/learningkotlin/controller/AnimeController.kt
+++ src/main/kotlin/com/example/learningkotlin/controller/AnimeController.kt
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 import javax.validation.Valid
 
-
 @RestController
 @RequestMapping("animes")
 class AnimeController(
